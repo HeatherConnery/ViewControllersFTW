451a841b1c26597d1139088483a1bfbb8008ad43
HeatherConnery <heatherconnery@gmail.com>
2015-10-23 17:08:02 -0400

    almost done

--- ViewControllersFTWApp/ViewControllersFTWApp/ViewController.swift
+++ ViewControllersFTWApp/ViewControllersFTWApp/ViewController.swift
@@ -13,11 +13,43 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("This is the viewDidLoad call")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewDidLoad()
+        print("This is the viewWillAppear call")
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidLoad()
+        print("This is the viewDidAppear call")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewDidLoad()
+        print("This is the viewWillLayoutSubviews call")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLoad()
+        print("This is the viewDidLayoutSubviews call")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewDidLoad()
+        print("This is the viewWillDisappear call")
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidLoad()
+        print("This is the viewDidDisappear call")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        print("This is the didReceiveMemoryWarning call")
     }
 
 
